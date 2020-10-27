5c44e047f6e1d2090e964a8b8eb8c33a0294a4b2
Alexey Baryzhikov <aleksiarts@gmail.com>
2020-10-28 00:30:48 +0300

    Add file upload

--- hello.py
+++ hello.py
@@ -1,27 +1,49 @@
 from flask import Flask
 from flask import request
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def show_home():
     return "Welcome to home page!"
 
+
 @app.route('/hello')
 def hello_world():
     return 'Hello, World!'
+
 
 @app.route('/user/<user_name>')
 def greet_user(user_name):
     return 'Hello, {}!'.format(user_name)
 
+
 @app.route('/post/<int:id>')
 def show_post(id):
     return 'Post ID {}'.format(id)
 
+
+def is_valid_login(login, password):
+    return login == 'alex' and password == '123'
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
     if request.method == 'POST':
-        return "Logging in..."
-    else :
-        return "Login form"
+        if is_valid_login(request.form['username'],
+                          request.form['password']):
+            return 'Logging in as {}...'.format(request.form['username'])
+        else:
+            return 'Login error!'
+    else:
+        return 'Please, provide credentials'
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    f = request.files['file']
+    f.save('{}/{}'.format(os.getcwd(), f.filename))
+    return 'Received file "{}"'.format(f.filename)
